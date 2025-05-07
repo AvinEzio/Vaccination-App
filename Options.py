5ec56fce09404b96268880ac34f96e1965b1cb0e
AvinEzio <91463716+AvinEzio@users.noreply.github.com>
2025-05-08 02:11:15 +0800

    Add files via upload

---
+++ Options.py
@@ -0,0 +1,18 @@
+def main_options():
+    print("__USER MENU__")
+    print("1.View User Info")
+    print("2.Update User Info")
+    print("3.View Vaccination Appointment Details")
+    print("4.RSVP Vaccine Appointment")
+    print("5.Quit To Main Menu")
+
+def admin_options():
+    print("__ADMIN MENU__\n1.View all users\n2.Update User Appointment\n3.Assign An Appointment\n4.View User Vaccination RSVP\n5.View Assigned Users\n6.View High Risk And Low Risk Users\n7.Quit To Main Menu")
+
+
+def update_user_options():
+    print("__User Update Menu__\n1.Update Personal Details\n2.Update Occupation\n3.Update Medical History\n4.Update Your COVID-19 Status\n5.Update Your Address\n6.Update All Information\n7.Back")
+
+def med_history_options():
+    print("Answer the following question to update medical history\n")
+    print("Have You Been In Close Contact With A COVID-19 Patient?")
